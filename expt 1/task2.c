073725f0d2a945ead5a26f4aa8a19696ba20b2be
Kamranshaikh31 <kamran31@ENG.RIZVI.EDU.IN>
2024-10-16 10:14:02 +0530

    Update task2.c

--- expt 1/task2.c
+++ expt 1/task2.c
@@ -1,29 +1,20 @@
 #include <stdio.h>
-
 int main() {
     int phy, chem, maths;
     float avg; 
-
     printf("+++ Student Eligibility Calculator +++\n");
-
     printf("Enter physics marks: ");
     scanf("%d", &phy);
-
     printf("Enter chemistry marks: ");
     scanf("%d", &chem);
-
     printf("Enter maths marks: ");
     scanf("%d", &maths);
-
     avg = (phy + chem + maths) / 3.0;
-
     printf("PCM Average = %.2f\n", avg);  
-
     if (avg > 50) {
         printf("You are eligible for admission.\n");
     } else {
         printf("You are not eligible for admission.\n");
     }
-
     return 0;
 }

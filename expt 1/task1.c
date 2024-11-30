842ec4f193e20bd0ea43391c8b9d60a22acdbaa5
Kamranshaikh31 <kamran31@ENG.RIZVI.EDU.IN>
2024-11-30 16:18:37 +0530

    Update task1.c

--- expt 1/task1.c
+++ expt 1/task1.c
@@ -1,11 +1,31 @@
-#include <stdio.h>
+/*
+    Expt. No : 1 , (task 1)
+    Title : WAP to print your name, age, class, division and UIN on the screen.
+     Use escape sequences to properly format the output.
+
+    Name: Shaikh Kamran Qamaralam
+    Class & Div: ECS-E
+    UIN: 241S012
+ */
+ #include <stdio.h>
 
 int main() {
+    //printing details
     printf("Full Name\t: Shaikh Kamran Qamaralam\n");
-    printf("Age\t\t: 19\n");
+    printf("Age\t\t\t: 19\n");
     printf("Class\t\t: FE-ECs\n");
     printf("Division\t: E\n");
-    printf("UIN\t\t: 2415012\n");
+    printf("UIN\t\t\t: 2415012\n");
     
     return 0;
 }
+/* 
+OUTPUT
+
+Full Name	: Shaikh Kamran Qamaralam
+Age			: 19
+Class		: FE-ECs
+Division	: E
+UIN			: 2415012
+
+*/

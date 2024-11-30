318c30df5c833f79da5a5c1fdc6ef08cf892f236
Kamranshaikh31 <kamran31@ENG.RIZVI.EDU.IN>
2024-11-30 16:48:09 +0530

    Update task1.c

--- expt 1/task1.c
+++ expt 1/task1.c
@@ -12,10 +12,10 @@
 int main() {
     //printing details
     printf("Full Name\t: Shaikh Kamran Qamaralam\n");
-    printf("Age\t\t\t: 19\n");
+    printf("Age\t\t: 19\n");
     printf("Class\t\t: FE-ECs\n");
     printf("Division\t: E\n");
-    printf("UIN\t\t\t: 2415012\n");
+    printf("UIN\t\t: 2415012\n");
     
     return 0;
 }

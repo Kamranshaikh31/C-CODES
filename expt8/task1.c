f03d81a0eb867dd5930380992282b3ff0e21c3ed
Kamranshaikh31 <kamran31@ENG.RIZVI.EDU.IN>
2024-12-01 21:28:29 +0530

    Update task1.c

--- expt8/task1.c
+++ expt8/task1.c
@@ -1,13 +1,40 @@
-//finding the length of a string without using library function.
+/*
+    Expt. No : 8 (task 1)
+    Title : WAP to  find the length of a string witout using a library function
+
+
+    Name: Shaikh Kamran Qamaralam 
+    Class & Div: ECS-E
+    UIN: 241S012  
+ */
 #include <stdio.h>
+
 int main() {
- char str[100];
- int length = 0;
- printf("Enter a string: ");
- scanf("%s", str);
- while (str[length] != '\0') {
- length++;
- }
- printf("The length of the string is: %d\n", length);
- return 0;
+    // Declare a character array to hold the string input
+    char str[100];
+
+    // Initialize an integer variable 'length' to store the length of the string
+    int length = 0;
+
+    // Prompt the user to enter a string
+    printf("Enter a string: ");
+    
+    // Read the string input from the user and store it in 'str'
+    scanf("%s", str);
+
+    // Loop to count the number of characters in the string
+    // The loop continues until it reaches the null-terminator '\0', which marks the end of the string
+    while (str[length] != '\0') {
+        length++; // Increment 'length' for each character in the string
+    }
+
+    // Print the length of the string
+    printf("The length of the string is: %d\n", length);
+
+    // Return 0 to indicate the program finished successfully
+    return 0;
 }
+/*OUTPUT
+Enter a string: abcde
+The length of the string is: 5
+*/
